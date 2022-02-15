c5a3e8ee3055c071a7d08174f7fc508994edde43
Your Name <you@example.com>
2022-02-15 15:54:22 +0300

    when admin loged in then he is loged in both websites

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@
 
   // this is to login if admin or editor has loged on the other admin panal.. in short, if an admin or editor has loged in to the admin panel, then he is automaticaly loged in to the user side of the website.
 
-  // this helps us to navigate back and forth b/n admin panel and the user side website for messaging and other stuff hasn't been discorverd.
+  // this helps us to navigate back and forth b/n admin panel and the user side website for messaging and other stuff hasn't been discorverd. 
   if(!isset($_SESSION['userId']) && isset($_SESSION['idz'])){
     echo 'in header';
     $_SESSION['userId'] = $_SESSION['idz'];
