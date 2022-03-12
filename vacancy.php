bd13e8978d161d483d6a3c345e57e19a1029e0f5
fonka <fikirgetahun97@gmail.com>
2022-03-12 20:27:42 +0300

    associative array implode and explode

--- vacancy.php
+++ vacancy.php
@@ -1,144 +1,79 @@
 <?php
-include "includes/header.php";
-include "includes/secnav.php";
-include "includes/navbar.php";
-	?>
-<div class="container">
-	<div class="row">
+$array = Array(
+  'foo' => 5,
+  'bar' => 12,
+  'baz' => 8
+);
+
+// $norm = array("ss","efd", "sdfq", "sdfw");
+// function array_map_assoc($array){
+//   $r = array();
+//   foreach ($array as $key=>$value)
+//     $r[$key] = "$key ($value)";
+//   return $r;
+// }
+
+// $x = implode(' , ', array_map_assoc($array));
+// // 
+// $y = explode(' , ',$x);
+// var_dump(array_map_assoc($array));
+
+// echo "ddddddddddd";
+// var_dump($y);
+
+
+// echo $y[0]
+
+// var_dump($norm)
+
+
+function implodeAssoc($glue,$arr)
+{
+   $keys=array_keys($arr);
+   $values=array_values($arr);
+
+   return(implode($glue,$keys).$glue.implode($glue,$values));
+
+   
+};
+
+//
+
+
+/**
+ * @name explodeAssoc($glue,$arr)
+ * @description makes an assiciative array from a string
+ * @parameter glue: the string to glue the parts of the array with
+ * @parameter arr: array to explode
+ */
+function explodeAssoc($glue,$str)
+{
+   $arr=explode($glue,$str);
+
+   $size=count($arr);
+
+   for ($i=0; $i < $size/2; $i++)
+       $out[$arr[$i]]=$arr[$i+($size/2)];  // here the out array key is the left side of the string which is determined by the loop counter being the half of the object count and the value of a key is always 1 shift after the half of the count.. this is clever
+
+   return($out);
+};
+
+
+$imp = implodeAssoc("%", $array);
+
+echo $imp;
+
+/// out put of $imp = foo%bar%baz%5%12%8  it separites the key at the left and values at right
+
+$exp = explodeAssoc("%", $imp);
+
+var_dump($exp)
+
+/// out put of $exp = array(3) { ["foo"]=> string(1) "5" ["bar"]=> string(2) "12" ["baz"]=> string(1) "8" } this is the constrarcted back associated array from string
+
+// echo $exp['foo']
 
 
 
-		<div class="col">
-			<ul class="nav flex-column">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled">Disabled</a>
-  </li>
-</ul>
-		</div>
 
-		<div class="col-md-8">
-			<div class="card">
-  <div class="card-header">
-    Finance Manager
-  </div>
-  <div class="card-body">
-    <div class="d-flex justify-content-between align-items-center">
-      <div>
-        <h5 class="card-title">Kenean Digital Technology</h5>
-     </div>
-      <small class="text-muted">Posted: <span class="text-success">12/09/21</span></small>
-    </div>
-    
-    <p class="card-text"><span class="fw-bolder">Job Description: </span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-    <p><small class="text-muted">Location:</small></p>
-    <p><small class="text-muted">Phone:</small></p>
-     <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="vacdescription.php" type="button" class="btn btn-sm btn-outline-primary">View</a>
-                  <a type="button" class="btn btn-sm btn-outline-warning">Fav</a>
-                </div>
-                <small class="text-muted">Deadline: <span class="text-danger">12/09/21</span></small>
-              </div>
-  </div>
-</div>
-
-	<div class="card">
-  <div class="card-header">
-    Finance Manager
-  </div>
-    <div class="card-body">
-    <div class="d-flex justify-content-between align-items-center">
-      <div>
-        <h5 class="card-title">Kenean Digital Technology</h5>
-     </div>
-      <small class="text-muted">Posted: <span class="text-success">12/09/21</span></small>
-    </div>
-    <p class="card-text"><span class="fw-bolder">Job Description: </span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-    <p><small class="text-muted">Location:</small></p>
-    <p><small class="text-muted">Phone:</small></p>
-     <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="vacdescription.php" type="button" class="btn btn-sm btn-outline-primary">View</a>
-                  <a type="button" class="btn btn-sm btn-outline-warning">Fav</a>
-                </div>
-                <small class="text-muted">Deadline: <span class="text-danger">12/09/21</span></small>
-              </div>
-  </div>
-</div>
-	<div class="card">
-  <div class="card-header">
-    Marketing Manager
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Kenean Digital Technology</h5>
-    <p class="card-text"><span class="fw-bolder">Job Description: </span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-    <p><small class="text-muted">Location:</small></p>
-    <p><small class="text-muted">Phone:</small></p>
-     <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="vacdescription.php" type="button" class="btn btn-sm btn-outline-primary">View</a>
-                  <a type="button" class="btn btn-sm btn-outline-warning">Fav</a>
-                </div>
-                <small class="text-muted">Deadline: <span class="text-danger">12/09/21</span></small>
-              </div>
-  </div>
-</div>
-	<div class="card">
-  <div class="card-header">
-    Finance Manager
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Kenean Digital Technology</h5>
-    <p class="card-text"><span class="fw-bolder">Job Description: </span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-    <p><small class="text-muted">Location:</small></p>
-    <p><small class="text-muted">Phone:</small></p>
-     <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="vacdescription.php" type="button" class="btn btn-sm btn-outline-primary">View</a>
-                  <a type="button" class="btn btn-sm btn-outline-warning">Fav</a>
-                </div>
-                <small class="text-muted">Deadline: <span class="text-danger">12/09/21</span></small>
-              </div>
-  </div>
- 
-</div>
-  <div class="container mx-auto">
-         <nav aria-label="Page navigation example" >
-  <ul class="pagination">
-    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item"><a class="page-link" href="#">Next</a></li>
-  </ul>
-</nav>
-    </div>
-		</div>
- 
-       
-		<div class="col">
-
-			<div class="card" style="width: 18rem;">
-  <img src="assets/img/work-7.jpg" class="card-img-top" alt="...">
-</div>
-		</div>
-		
-	</div>
-	  
-
-</div>
-
-
-
-	<?php
-include "includes/footer.php";
 ?>
